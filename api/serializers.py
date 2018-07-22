7af883e477d4616264ecd7de91546e920ccf13d2
zalrajaan <z.alrajaan@gmail.com>
2018-07-22 20:30:11 +0300

    done

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,18 @@ class RestaurantListSerializer(serializers.ModelSerializer):
         	'opening_time',
         	'closing_time',
         	]
+
+class Anything2(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['id', 'owner', 'name', 'description', 'opening_time','closing_time']
+
+class Anything3(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['name','description', 'opening_time','closing_time']
+
+class Anything4(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,20 +1,30 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,RetrieveAPIView ,DestroyAPIView, RetrieveUpdateAPIView
 from restaurants.models import Restaurant
-from .serializers import RestaurantListSerializer
+from .serializers import RestaurantListSerializer,Anything2, Anything3, Anything4
 
 class RestaurantListView(ListAPIView):
-	queryset = Restaurant.objects.all()
-	serializer_class = RestaurantListSerializer
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantListSerializer
 
 # Complete me
-class RestaurantDetailView():
+class RestaurantDetailView(RetrieveAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = Anything2
+    lookup_field = 'id'
+    lookup_url_kwarg = 'restaurant_id'
 
+# Complete me
+class RestaurantUpdateView(RetrieveUpdateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = Anything3
+    lookup_field = 'id'
+    lookup_url_kwarg = 'restaurant_id'
 
 
 # Complete me
-class RestaurantUpdateView():
+class RestaurantDeleteView(DestroyAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = Anything4
+    lookup_field = 'id'
+    lookup_url_kwarg = 'restaurant_id'
 
-
-
-# Complete me
-class RestaurantDeleteView():
